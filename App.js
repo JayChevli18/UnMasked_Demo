737f342fea89d7a574ab5bd35361f4cf0481b62f
JayChevli18 <chevlijay70@gmail.com>
2024-06-11 10:01:34 +0530

    Splash Screen

--- App.js
+++ App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView, StyleSheet, Image, ImageBackground, View, Text, Touchable, TouchableOpacity, TouchableHighlight } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import { AccountNavigation } from './src/navigation/account_navigation';
+import { useSafeAreaFrame } from 'react-native-safe-area-context';
+import SplashScreen from './src/screens/SplashScreen';
 
 const App = () => {
+
+  const [isLoading, setIsLoading]=useState(true);
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      setIsLoading(false);
+    },1000)
+  },[]);
+
+  if(isLoading)
+  {
+    return <SplashScreen></SplashScreen>
+  }
+
   return (
       <AccountNavigation></AccountNavigation>
   );
