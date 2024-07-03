378d8bb5ebd74eb251b23ba6664557376b59a741
JayChevli18 <chevlijay70@gmail.com>
2024-07-03 15:55:30 +0530

    Caption Added - AddNewPostDetaisScreen

--- src/screens/AddNewPostDetailsScreen.js
+++ src/screens/AddNewPostDetailsScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { SafeAreaView, Text, ScrollView, Image, View, StyleSheet, Dimensions, Switch, TouchableHighlight } from "react-native";
+import { SafeAreaView, Text, ScrollView, Image, View, TextInput, StyleSheet, Dimensions, Switch, TouchableHighlight } from "react-native";
 import LinearGradient from "react-native-linear-gradient";
 import { Dropdown } from "react-native-element-dropdown";
 import ToggleSwitch from "toggle-switch-react-native";
@@ -14,74 +14,90 @@ export const AddNewPostDetailsScreen = ({ navigation, route }) => {
     const [isEnabled, setIsEnabled] = useState(false);
 
     return (
-        <SafeAreaView style={{ flex: 1 }}>
-            <View>
-                <ScrollView horizontal style={{ padding: 20, gap: 20, }}>
-                    {selectedImages.map((image, index) => (
-                        <View style={{ marginRight: 40, width: windowWidth - 40, height: 400, overflow: "hidden" }} key={index}>
-                            <Image source={{ uri: image.uri }} style={styles.fullSelectedImage} />
-                        </View>
-                    ))}
-                </ScrollView>
-                <View style={{ flexDirection: "row", padding: 20, justifyContent: "space-between", alignItems: "center" }}>
-                    <Text>Show post to</Text>
-                    <LinearGradient start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }} colors={["#FBB4D1", "#BF9EF2"]} style={{ width: "40%", borderRadius: 8 }}>
-                        <Dropdown
-                            data={options}
+        <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+            <ScrollView>
+                <View>
+                    <View style={{ paddingLeft: 20, paddingRight: 20, flex:1 }}>
+                        <TextInput
+                            placeholder="Add your caption here!"
+                            multiline={true}
+                            editable={true}
+                            numberOfLines={1}
                             style={{
-                                width: "100%",
-                                height: 40,
-                                borderColor: 'gray',
-                                borderWidth: 0.5,
-                                borderRadius: 8,
-                                paddingHorizontal: 8,
+                                textAlignVertical: 'top',
+                                fontSize: 16,
+                                backgroundColor: "white"
                             }}
-                            placeholderStyle={{ fontSize: 16, color: "white" }}
-                            labelField="label"
-                            valueField="value"
-                            selectedTextStyle={{ color: "white" }}
-                            iconColor="white"
-                            containerStyle={{ maxHeight: "70%" }}
-                            value={value}
-                            onChange={item => {
-                                setValue(item.value);
-                            }}
-                        ></Dropdown>
-                    </LinearGradient>
-                </View>
-                <View style={{ flexDirection: "row", padding: 20, justifyContent: "space-between", alignItems: "center" }}>
-                    <Text>Schedule post</Text>
-                    <ToggleSwitch
-                        isOn={isEnabled}
-                        offColor="#FBB4D1"
-                        onColor="#BF9EF2"
-                        size="medium"
-                        onToggle={isOn => { console.log(isOn); setIsEnabled(isOn) }}
-                    />
-                </View>
-            </View>
-            <View style={{ flexDirection: "row", justifyContent: "space-between", position: "absolute", bottom: 0, left: 0, right: 0 , padding:20}}>
-                <View style={{ flex: 1, marginRight: 20 }}>
-                    <LinearGradient start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }} colors={["#FBB4D1", "#BF9EF2"]} style={{ borderRadius: 10, alignItems: "center", height: 40, justifyContent: "center", marginTop: 15, }}>
-                        <TouchableHighlight underlayColor="#BF9EF2" style={{ borderRadius: 10, backgroundColor: "transparent", alignItems: "center", height: 40, justifyContent: "center", width: "100%" }} onPress={() => { console.log("ok") }}>
-                            <View>
-                                <Text style={{ color: "white", fontSize: 18 }}>Send Privately</Text>
-                            </View>
-                        </TouchableHighlight>
-                    </LinearGradient>
-                </View>
-                <View style={{ flex: 1 }}>
-                    <LinearGradient start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }} colors={["#FBB4D1", "#BF9EF2"]} style={{ borderRadius: 10, alignItems: "center", height: 40, justifyContent: "center", marginTop: 15, width: "100%" }}>
-                        <TouchableHighlight underlayColor="#BF9EF2" style={{ borderRadius: 10, backgroundColor: "transparent", alignItems: "center", height: 40, justifyContent: "center", width: "100%" }} onPress={() => { console.log("ok") }}>
-                            <View>
-                                <Text style={{ color: "white", fontSize: 18 }}>{isEnabled ? "Schedule" : "Post Now" }</Text>
-                            </View>
-                        </TouchableHighlight>
-                    </LinearGradient>
-                </View>
-            </View>
+                        />
 
+                    </View>
 
+                    <ScrollView horizontal style={{ paddingLeft: 20,paddingRight:20, gap: 20, }}>
+                        {selectedImages.map((image, index) => (
+                            <View style={{ marginRight: 40, width: windowWidth - 40, height: 400, overflow: "hidden" }} key={index}>
+                                <Image source={{ uri: image.uri }} style={styles.fullSelectedImage} />
+                            </View>
+                        ))}
+                    </ScrollView>
+                    <View style={{ flexDirection: "row", padding: 20, justifyContent: "space-between", alignItems: "center" }}>
+                        <Text>Show post to</Text>
+                        <LinearGradient start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }} colors={["#FBB4D1", "#BF9EF2"]} style={{ width: "40%", borderRadius: 8 }}>
+                            <Dropdown
+                                data={options}
+                                style={{
+                                    width: "100%",
+                                    height: 40,
+                                    borderColor: 'gray',
+                                    borderWidth: 0.5,
+                                    borderRadius: 8,
+                                    paddingHorizontal: 8,
+                                }}
+                                placeholderStyle={{ fontSize: 16, color: "white" }}
+                                labelField="label"
+                                valueField="value"
+                                selectedTextStyle={{ color: "white" }}
+                                iconColor="white"
+                                containerStyle={{ maxHeight: "70%" }}
+                                value={value}
+                                onChange={item => {
+                                    setValue(item.value);
+                                }}
+                            ></Dropdown>
+                        </LinearGradient>
+                    </View>
+                    <View style={{ flexDirection: "row", padding: 20, justifyContent: "space-between", alignItems: "center" }}>
+                        <Text>Schedule post</Text>
+                        <ToggleSwitch
+                            isOn={isEnabled}
+                            offColor="#FBB4D1"
+                            onColor="#BF9EF2"
+                            size="medium"
+                            onToggle={isOn => { console.log(isOn); setIsEnabled(isOn) }}
+                        />
+                    </View>
+                </View>
+                <View style={{ flexDirection: "row", justifyContent: "space-between", padding: 20 }}>
+                    <View style={{ flex: 1, marginRight: 20 }}>
+                        <LinearGradient start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }} colors={["#FBB4D1", "#BF9EF2"]} style={{ borderRadius: 10, alignItems: "center", height: 40, justifyContent: "center", marginTop: 15, }}>
+                            <TouchableHighlight underlayColor="#BF9EF2" style={{ borderRadius: 10, backgroundColor: "transparent", alignItems: "center", height: 40, justifyContent: "center", width: "100%" }} onPress={() => { console.log("ok") }}>
+                                <View>
+                                    <Text style={{ color: "white", fontSize: 18 }}>Send Privately</Text>
+                                </View>
+                            </TouchableHighlight>
+                        </LinearGradient>
+                    </View>
+                    <View style={{ flex: 1 }}>
+                        <LinearGradient start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }} colors={["#FBB4D1", "#BF9EF2"]} style={{ borderRadius: 10, alignItems: "center", height: 40, justifyContent: "center", marginTop: 15, width: "100%" }}>
+                            <TouchableHighlight underlayColor="#BF9EF2" style={{ borderRadius: 10, backgroundColor: "transparent", alignItems: "center", height: 40, justifyContent: "center", width: "100%" }} onPress={() => { console.log("ok") }}>
+                                <View>
+                                    <Text style={{ color: "white", fontSize: 18 }}>{isEnabled ? "Schedule" : "Post Now"}</Text>
+                                </View>
+                            </TouchableHighlight>
+                        </LinearGradient>
+                    </View>
+                </View>
+
+            </ScrollView>
         </SafeAreaView>
     )
 }
